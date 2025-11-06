79ac25194c9deb29525f2d244cc895ea673e17a5
pdahika <124161207+pdahika@users.noreply.github.com>
2025-11-06 18:56:18 +0530

    Update output for node_group_id in output.tf

--- Mega-Project-Terraform/Mega-Project-Terraform-main/output.tf
+++ Mega-Project-Terraform/Mega-Project-Terraform-main/output.tf
@@ -1,9 +1,11 @@
+# output.tf
+
 output "cluster_id" {
   value = aws_eks_cluster.devopsshack.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.devopsshack_node_group.id
 }
 
 output "vpc_id" {
@@ -13,3 +15,5 @@ output "vpc_id" {
 output "subnet_ids" {
   value = aws_subnet.devopsshack_subnet[*].id
 }
+
+
