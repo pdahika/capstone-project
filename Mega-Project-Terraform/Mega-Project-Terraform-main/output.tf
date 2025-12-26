30684eb7ef02f1ab800555bf89d16542af7f4bcc
pdahika <124161207+pdahika@users.noreply.github.com>
2025-12-26 17:37:13 +0530

    Update output values for new EKS resources

--- Mega-Project-Terraform/Mega-Project-Terraform-main/output.tf
+++ Mega-Project-Terraform/Mega-Project-Terraform-main/output.tf
@@ -1,19 +1,19 @@
 # output.tf
 
 output "cluster_id" {
-  value = aws_eks_cluster.devopsshack.id
+  value = aws_eks_cluster.devopsshack1.id
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack_node_group.id
+  value = aws_eks_node_group.devopsshack1_node_group.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.devopsshack1_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.devopsshack1_subnet[*].id
 }
 
 
